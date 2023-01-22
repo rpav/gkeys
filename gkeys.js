1a02a2f07beaac1fd0c1f336e588156413764637
Ryan Pavlik <rpavlik@gmail.com>
2023-01-21 19:09:36 -0600

    basic craptastic gathering of all the pieces

--- gkeys.js
+++ gkeys.js
@@ -1,19 +1,43 @@
 #!/usr/bin/env node
 
-const HID        = require('node-hid');
-const {say, str} = require("./util.js");
+const HID = require('node-hid');
+const { say, str } = require("./util.js");
+const { Hardware } = require('keysender');
 
-let devices    = HID.devices();
-let deviceInfo = devices.find(function(d) {
-    var isTeensy = d.vendorId === 0x03a8 && d.productId === 0xa649;
-    return isTeensy && d.usagePage === 0xFF60 && d.usage === 0x61;
+const path = require('path');
+const ps = require('process');
+const { fork } = require('child_process');
+
+let devices = HID.devices();
+let deviceInfo = devices.find(function (d) {
+    return d.vendorId === 0x03a8 && d.productId === 0xa649 &&
+        d.usagePage === 0xFF60 && d.usage === 0x61;
 });
 
-if(deviceInfo) {
+let ww;
+
+const watchPath = path.resolve('windowWatcher.js');
+const watchProcess = fork(watchPath, [], 
+    {stdio: ['pipe', 'pipe', 'pipe', 'ipc']});
+
+watchProcess.on('message', msg => {
+    say("msg: ", JSON.parse(msg));
+});
+
+if (deviceInfo) {
     const kb = new HID.HID(deviceInfo.path);
 
-    kb.on("data", function(data) { say(data); });
-    kb.on("error", function(err) { say("error ", err); })
+    kb.on("data", function (data) {
+        let kx   = data[0];
+        let ky   = data[1];
+        let bits = data[2];
+
+        if(ww && ww.workwindow.isForeground()) {
+            ww.keyboard.toggleKey("left", !!(bits & 1));
+        }
+        say(data); 
+    });
+    kb.on("error", function (err) { say("error ", err); })
 } else {
     say("No device found.");
 }
---
+++ windowWatcher.js
@@ -0,0 +1,66 @@
+const { say, str } = require("./util.js");
+const ps = require('process');
+
+const { Kernel32, User32 } = require('win32-api');
+const ffi = require('ffi-napi');
+const ref = require('ref-napi');
+const iconv = require('iconv-lite');
+const StructDi = require('ref-struct-di');
+const { DModel: M, DStruct: DS, DTypes: W } = require('win32-api');
+
+const Struct = StructDi(ref);
+
+const knl32 = Kernel32.load();
+const user32 = User32.load();
+
+const titleBuffer = Buffer.alloc(256);
+const classBuffer = Buffer.alloc(256);
+
+const user32ex = ffi.Library("user32", {
+    GetWindowTextW: ["int", ["pointer", "pointer", "int"]],
+    GetClassNameW: ["int", ["pointer", "pointer", "int"]],
+});
+
+function findProfileFor(w, c) {
+    return str("profiles/", c);
+}
+
+function sendMessage(msg) {
+    if(ps.send) ps.send(msg);
+}
+
+const cb = ffi.Callback("void", ["pointer", "int", "pointer", "long", "long", "int", "int"],
+    function(hWinEventHook, event, hwnd, idObject, idChild, idEventThread, dwmsEventTime) {
+        let hwndNum = hwnd.address();
+        let titleLen = user32ex.GetWindowTextW(hwnd, titleBuffer, titleBuffer.length);
+        let classLen = user32ex.GetClassNameW(hwnd, classBuffer, classBuffer.length);
+        let winTitle = iconv.decode(titleBuffer.subarray(0, titleLen * 2), 'ucs2');
+        let winClass = iconv.decode(classBuffer.subarray(0, classLen * 2), 'ucs2');
+
+        let profileName = findProfileFor(winTitle, winClass);
+        let msg = {
+            profile: profileName,
+            hwnd: hwndNum,
+        };
+        sendMessage(JSON.stringify(msg));
+    },
+);
+
+
+function win32EventLoop() {
+    const msg = new Struct(DS.MSG)();
+    const pt = new Struct(DS.POINT)();
+
+    msg.pt = pt.ref();
+
+    user32.SetWinEventHook(3, 3, 0, cb, 0, 0, 2);
+    say("ok...");
+    while(user32.GetMessageW(msg.ref(), 0, 0, 0)) {
+        say("msg:", msg);
+        user32.TranslateMessageEx(msg.ref());
+        user32.DispatchMessageW(msg.ref());
+    }
+
+    say("all done");
+}
+win32EventLoop();
