0724e7688af63a6dc23b65618b1d22aa876d714d
Ryan Pavlik <rpavlik@gmail.com>
2023-01-22 20:24:26 -0600

    Work toward 'simple' api for key assignment
    - support object events with obj.exec
    - tog
    
    Still need a better way to refer to the same object on other layers

--- gkeys-api.js
+++ gkeys-api.js
@@ -1,3 +1,5 @@
+const {say} = require('./lib/util.js');
+
 let BUNDLE;
 
 function _setBundle(b) {
@@ -18,7 +20,33 @@ class GKeys {
     }
 }
 
+class ToggleLayer {
+    constructor(name) {
+        this.name = name;
+    }
+
+    exec(state) {
+        const pm = BUNDLE.profileManager;
+
+        if(!state) return;
+
+        if(this.old) {
+            pm.setLayer(pm.findLayer(this.old));
+            this.old = null;
+            return;
+        }
+
+        this.old = pm.currentLayer();
+        pm.setLayer(pm.findLayer(this.name));
+    }
+}
+
+function tog(name) {
+    return new ToggleLayer(name);
+}
+
 module.exports = {
     _setBundle,
     gkeys : new GKeys(),
+    tog,
 };
