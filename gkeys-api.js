6c4f835f8f83cad60227532e565c315dc1aeca8c
Ryan Pavlik <rpavlik@gmail.com>
2023-01-23 19:32:34 -0600

    tog / togback working; profile data
    - tog(name) / togback() work as functions when called in layout
    - "current profile data" for storing state between presses; set up
      to be available during profile loading and preserved while active

--- gkeys-api.js
+++ gkeys-api.js
@@ -7,6 +7,8 @@ function _setBundle(b) {
         BUNDLE = b;
 }
 
+function data() { return BUNDLE.profileManager.currentProfileData(); }
+
 class GKeys {
     setLayer(name) {
         const pm = BUNDLE.profileManager;
@@ -18,35 +20,67 @@ class GKeys {
         }
         return false;
     }
+
+    currentLayer() {
+        const pm = BUNDLE.profileManager;
+        return pm.currentLayer();
+    }
+}
+
+const ToggleLayerS = Symbol.for("ToggleLayer");
+
+function ToggleLayerSetup() {
+    const d = data();
+    if(!d[ToggleLayerS])
+        d[ToggleLayerS] = {
+            stack : [],
+            togBack : new ToggleLayerBack(),
+        };
 }
 
 class ToggleLayer {
     constructor(name) {
         this.name = name;
+        ToggleLayerSetup();
     }
 
     exec(state) {
-        const pm = BUNDLE.profileManager;
-
-        if(!state) return;
-
-        if(this.old) {
-            pm.setLayer(pm.findLayer(this.old));
-            this.old = null;
+        if(!state)
             return;
-        }
 
-        this.old = pm.currentLayer();
+        const pm = BUNDLE.profileManager;
+        const d = data();
+
+        d[ToggleLayerS].stack.push(pm.currentLayer());
         pm.setLayer(pm.findLayer(this.name));
     }
 }
 
-function tog(name) {
-    return new ToggleLayer(name);
+class ToggleLayerBack {
+    exec(state) {
+        say("back?");
+        if(!state)
+            return;
+
+        const pm = BUNDLE.profileManager;
+        const d = data();
+        const stack = d[ToggleLayerS].stack;
+
+        if(stack.length == 0)
+            return;
+
+        let old = stack.pop();
+        pm.setLayer(pm.findLayer(old));
+    }
+}
+
+function tog(name) { return new ToggleLayer(name); }
+function togback() {
+    const d = data();
+    return d[ToggleLayerS].togBack;
 }
 
 module.exports = {
     _setBundle,
-    gkeys : new GKeys(),
-    tog,
+    gkeys : new GKeys(), tog, togback,
 };
