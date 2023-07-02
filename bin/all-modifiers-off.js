6d62478f8da871f44d51424869f9d01ba2420b86
Ryan Pavlik <rpavlik@gmail.com>
2023-07-02 09:04:17 -0500

    keysender 2.3.0; fix breaking changes

--- bin/all-modifiers-off.js
+++ bin/all-modifiers-off.js
@@ -7,7 +7,7 @@ let hwnd = user32ex.GetForegroundWindow().address();
 let sendkey = new Hardware(hwnd);
 
 const keys = [
-    'alt', 'ctrl', 'lalt', 'ralt', 'lctrl', 'rctrl', 'lshift', 'rshift',
+    'alt', 'ctrl', 'lAlt', 'rAlt', 'lCtrl', 'rCtrl', 'lShift', 'rShift', 'capsLock'
 ];
 
 for(const k of keys) sendkey.keyboard.toggleKey(k, false);
